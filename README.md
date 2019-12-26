1c7d88f403b8d0e579c240a6fe2f37c241f107a8
yangfukai <yangfukai@qiyi.com>
2019-12-26 18:21:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Runtime编译环境
+
+## 源码下载
+[官网](https://opensource.apple.com/source/objc4/)
+
+> 将url中的source改成tarballs即可看到所有的压缩包
+
+
+
+## 参考
+[iOS_objc4-756.2 最新源码编译调试](https://juejin.im/post/5d9c829df265da5ba46f49c9)
---
+++ objc4-756.2/RuntimeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RuntimeTest
+//
+//  Created by yangfukai on 2019/12/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
